a679bf940bdd7a5743d04d00c26775ac80ef84f7
h4w5 <philip@h4w5.com>
2015-05-11 00:37:51 -0400

    link to gh oauth identity provision service

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var request = require('request');
 
 var app = express();
 
+var GITHUB_OAUTH_ID     = process.env.GITHUB_OAUTH_ID
+var GITHUB_OAUTH_SECRET = process.env.GITHUB_OAUTH_SECRET
+
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
@@ -17,10 +20,14 @@ app.use(session({
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-  res.render(
-    'index',
-    {link: 'https://github.com/login/oauth/authorize'}
-  )
+  var gitHubPath  = 'https://github.com/login/oauth/authorize'
+  var redirectUrl = 'http://localhost:9888/oauth_callback'
+
+  var gitHubOauthUrl = gitHubPath +
+      '?client_id=' + encodeURIComponent(GITHUB_OAUTH_ID) +
+      '&redirect_url=' + encodeURIComponent(redirectUrl)
+
+  res.render('index', {gitHubOauthUrl: gitHubOauthUrl})
 });
 
 app.listen(9888, function() {
