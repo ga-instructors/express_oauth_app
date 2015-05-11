5dc293af0c3a428297b386d8bf291792b42b8c88
h4w5 <philip@h4w5.com>
2015-05-11 02:12:23 -0400

    get access token from github and user info associated with it

--- server.js
+++ server.js
@@ -24,10 +24,57 @@ app.get('/', function(req, res) {
   var redirectUrl = 'http://localhost:9888/oauth_callback'
 
   var gitHubOauthUrl = gitHubPath +
-      '?client_id=' + encodeURIComponent(GITHUB_OAUTH_ID) +
+      '?client_id='    + GITHUB_OAUTH_ID +
       '&redirect_url=' + encodeURIComponent(redirectUrl)
 
   res.render('index', {gitHubOauthUrl: gitHubOauthUrl})
+});
+
+app.get('/oauth_callback', function(req, res) {
+  var gitHubPath  = 'https://github.com/login/oauth/access_token'
+  var redirectUrl = 'http://localhost:9888/oauth_callback'
+
+  request({
+    url:    gitHubPath,
+    method: 'POST',
+    json:   true,
+    body:   {
+      client_id:     GITHUB_OAUTH_ID,
+      client_secret: GITHUB_OAUTH_SECRET,
+      code:          req.query.code,
+      redirect_uri:  redirectUrl
+    }
+  }, function(err, response, body) {
+    req.session.access_token = body.access_token
+    res.redirect('/complete');
+  });
+});
+
+app.get('/complete', function(req, res) {
+  var access_token = req.session.access_token
+  console.log(access_token)
+  if (!req.session.name) {
+    request({
+      url:    'https://api.github.com/user',
+      json:    true,
+      headers: {
+        'Authorization': 'token ' + access_token,
+        'User-Agent': 'request'
+      }
+    }, function(err, response, body) {
+      req.session.name  = body.name
+      req.session.email = body.email
+      res.render('complete', {
+        name:  req.session.name,
+        email: req.session.email
+      });
+    });
+  } else {
+    res.render('complete', {
+      name:  req.session.name,
+      email: req.session.email
+    });
+  }
 });
 
 app.listen(9888, function() {
